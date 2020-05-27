c6bd0e21ca54e31c289a0a16df6604692512f0f6
chilindrina-03 <66022553+chilindrina-03@users.noreply.github.com>
2020-05-27 10:37:47 -0500

    cambio 2
    ejercicios de estilos de programación

---
+++ ejercicio estilos de programacion.py
@@ -0,0 +1,36 @@
+
+ESPACIO EN BLANCO 
+
+def NOTAS(notaprimerparcia,notasegundoparcial):
+    res=notaprimercial+notasegundoparcial
+    prueba(notauno,notados)
+
+
+
+def prueba (notaprimerparcial, notasegundoparcial) :
+    res = notaprimerparcial + notasegundoparcial
+    return (notauno, notados)
+
+
+
+VARIABLES
+
+resultadosegundoparcial = 4.5
+
+R = 4.5
+
+
+resultado_segundo_parcial = 4.5
+
+resultadoSegundoParcial = 4.5
+
+
+COSTANTES
+
+
+RESULTADOSEGUNDOPARCIAL = "4.5!"
+
+resultadosegundoparcial = "4.5!"
+
+
+RESULTADO_SEGUNDO_PARCIAL = "4.5!"
